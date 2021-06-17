4f4a34bbbe494a018d28dffe9dca982e565d0bb8
ombahiwal@gmail.com <ombahiwal@gmail.com>
2021-06-17 20:41:52 +0530

    env api url -autodeploy

--- src/services/env-vars.js
+++ src/services/env-vars.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    // API_URL:"https://api.tabme.io",
-    API_URL:"http://localhost:8000",
+    API_URL:"https://api.tabme.io",
+    // API_URL:"http://localhost:8000",
     CDN_URL:"https://cdn.tabme.io",
     STRIPE_PK_LIVE:"pk_live_51I0nwkJFWMf2kdoDzVVKDvnUDkvi1aAkCvskzcZx3ePYco9nYbcpAlCLFoqi793rdPTUc8TK3KCdnG7MGEXy10IK00kwK9ATB9",
     STRIPE_PK_TEST:"pk_test_51I0nwkJFWMf2kdoDpjZaGXgo5TG6GbKs7okf9Qy2PBQxS7aKEhOA7d6wEKRTh6xiYk4te6Mtby9N8mVgfsTElcUw00gRtG7XN3",
